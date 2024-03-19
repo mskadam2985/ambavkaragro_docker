702c1fcd5304df7d840254587f5d0d1bdd04d1e3
mskadam2985 <mskadam2985@gmail.com>
2024-03-20 00:34:09 +0530

    changeAllowhost

--- ambavkaragro_web/ambavkaragro/settings.py
+++ ambavkaragro_web/ambavkaragro/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['','www.ambavkaragro.com','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['65.0.55.87','www.ambavkaragro.com','127.0.0.1','localhost']
 
 
 # Application definition
